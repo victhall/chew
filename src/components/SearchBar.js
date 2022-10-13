8b8c887c49966454fd507d0e88acc0af5656fdd4
victhall <victoriahall.dev@gmail.com>
2022-10-13 14:28:18 -0400

    implement search function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,18 +1,31 @@
-import { useState } from 'react';
+import { useReducer, useState } from 'react';
 import classes from './SearchBar.module.css';
 import { IoSearchOutline, IoCloseOutline } from "react-icons/io5";
 
 export default function SearchBar() {
+  const [search, setSearch] = useState('');
+
+  function searchHandler(event) {
+    setSearch(event.target.value);
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+  };
+
+  function clearSearch() {
+    setSearch('')
+  };
 
   return (
     <div className={classes['form-container']}>
-      <form onSubmit="">
+      <form onSubmit={submitHandler}>
         <input
           placeholder='Restaurant or Cuisine'
           type='text'
-          onChange="" />
-          <button className={classes['search-btn']}><IoSearchOutline /></button>
-          <button className={classes['close-btn']}><IoCloseOutline /></button>
+          onChange={searchHandler} />
+        <button className={classes['search-btn']}><IoSearchOutline /></button>
+        <button className={classes['close-btn']}><IoCloseOutline /></button>
       </form>
     </div>
   )
