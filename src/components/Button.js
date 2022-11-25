d6d7667047b7f509e659b987a0b6aea7fe60d8fe
victhall <victoriahall.dev@gmail.com>
2022-11-25 15:53:35 -0500

    fix css on sidebar

---
+++ src/components/Button.js
@@ -0,0 +1,7 @@
+export default function Button(props) {
+  return (
+    <button>
+      {props.children}
+    </button>
+  )
+}
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,19 @@
 import classes from './Sidebar.module.css';
+import SearchBar from './SearchBar';
+import Button from './Button';
 
 export default function Sidebar() {
-  return(
-    <section>
+  return (
+    <section className={classes.sidebar}>
       
+      <div>
+      <div className={classes.navigation}>
+      <Button className={classes['nav-btn']}>Search</Button>
+      <Button className={classes['nav-btn']}>Favourites</Button>
+      </div>
+
+      <SearchBar />
+      </div>
     </section>
   )
 }
