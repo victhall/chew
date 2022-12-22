17f083e2473501309d79c81c7aceba740a3f10da
victhall <victoriahall.dev@gmail.com>
2022-12-21 23:57:18 -0500

    organize/view restaurants based on collection name

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import CollectionContext from './store/collection-context';
 import CollectionRestaurant from './CollectionRestaurant';
 
@@ -10,26 +10,32 @@ export default function Collection() {
   //   CollectionContext.saveRestaurant(collectionRestaurant);
   // };
 
-//   Get collection names, save in an array,
-// Loop through categories, 
-//render restaurants under categories of category name === collection name
+  let groupedCollections = collectionCtx.restaurants.reduce((groupedObj, item) => {
+
+    groupedObj[item.collectionName] = groupedObj[item.collectionName] || [];
+
+    groupedObj[item.collectionName].push(item)
+    console.log('I RAN')
+    return groupedObj;
+  }, {})
 
 
+  
   return (<ul>
-    {collectionCtx.restaurants && collectionCtx.restaurants.map((restaurant) => {
-      return <CollectionRestaurant
-        name={restaurant.name}
-        address={restaurant.address}
-        city={restaurant.city}
-        province={restaurant.province}
-        zip_code={restaurant.zip_code}
-        price={restaurant.price}
-        collectionName={restaurant.collectionName}
-        // onAdd={saveRestaurantHandler.bind(null, { ...restaurant})}
+
+     <CollectionRestaurant
+      collectionData={groupedCollections}
+        // name={restaurant.name}
+        // address={restaurant.address}
+        // city={restaurant.city}
+        // province={restaurant.province}
+        // zip_code={restaurant.zip_code}
+        // price={restaurant.price}
+        // collectionName={restaurant.collectionName}
+        // categories={categories}
+      // onAdd={saveRestaurantHandler.bind(null, { ...restaurant})}
       />
-    })}
+    
   </ul>)
-
-
 
 }
--- src/components/CollectionRestaurant.js
+++ src/components/CollectionRestaurant.js
@@ -2,12 +2,25 @@ import classes from './CollectionRestaurant.module.css'
 
 export default function CollectionRestaurant(props) {
 
+
+
+console.log('dataa', props.collectionData)
+
   return (
-    <div>   
-      <p>{props.collectionName}</p>   
+    <div>
+      {Object.keys(props.collectionData).map((key, y) =>
+        <div key={y}>
+           <h3>{key}</h3>
+            {props.collectionData[key].map((item, y) =>
+            <div key={y}>{item.name} {item.price}</div>
+            )}
+        </div>
+      )}
+
+      {/* <p>{props.collectionName}</p>
       <h2 className={classes['restaurant-name']}>{props.name}</h2>
       <p className={classes['restaurant-address']}>{props.price}</p>
-      <p className={classes['restaurant-address']}>{props.address}, {props.city}, {props.province} {props.zip_code}</p>
-      </div>
+      <p className={classes['restaurant-address']}>{props.address}, {props.city}, {props.province} {props.zip_code}</p> */}
+    </div>
   )
 }
